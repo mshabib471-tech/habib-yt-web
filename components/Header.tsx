502c7b9a73ea61fce3da526408fa720d63df25c6
Habib-YT <mshabib471@gmail.com>
2025-11-11 15:28:02 +0600

    Add Header component with title 'Habib YT'

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-black/30 border-b border-gray-800 text-center py-4">
+      <h2 className="text-xl font-semibold text-primary">Habib YT</h2>
+    </header>
+  );
+}
